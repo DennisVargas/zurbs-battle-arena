7db36070db27a6bf253d75ddbd9f85f88832f5f9
ExtraordinaryBen <ben@powerpup.yi.org>
2016-10-14 18:27:21 -0700

    Initial commit

---
+++ desktop/src/com/teamawesome/zurbs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.teamawesome.zurbs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.teamawesome.zurbs.ZurbGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZurbGame(), config);
+	}
+}
