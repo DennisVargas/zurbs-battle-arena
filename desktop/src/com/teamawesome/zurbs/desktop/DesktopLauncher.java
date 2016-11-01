ed34f5d2fb977a297c79c40f7e17266d53b18b63
Dennis Vargas <dennis.vargas@wsu.edu>
2016-11-01 01:26:25 -0700

    Messed about with VisEditor and was able to import a True Type Font and edit the text. The "Text" also has an object ID but i'm not sure if it knows how big it is. The desktop launcher has been modified to launch MenuScene0 but the other two files from the core src folder are still available.

--- desktop/src/com/teamawesome/zurbs/desktop/DesktopLauncher.java
+++ desktop/src/com/teamawesome/zurbs/desktop/DesktopLauncher.java
@@ -2,6 +2,7 @@ package com.teamawesome.zurbs.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.teamawesome.zurbs.MenuScene0;
 import com.teamawesome.zurbs.ZurbGame;
 import com.teamawesome.zurbs.ZurbGameScene;
 
@@ -9,6 +10,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 //		new LwjglApplication(new ZurbGame(), config);
-		new LwjglApplication(new ZurbGameScene(), config);
+//		new LwjglApplication(new ZurbGameScene(), config);
+		new LwjglApplication(new MenuScene0(), config);
 	}
 }
