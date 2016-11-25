9e54723427997aff8af5640ab9adb960664e5382
Amy <amy.phillips@wsu.edu>
2016-11-24 22:22:10 -0800

    Adjusted music settings.

--- core/src/com/teamawesome/zurbs/SoundController.java
+++ core/src/com/teamawesome/zurbs/SoundController.java
@@ -14,7 +14,7 @@ public class SoundController {
 
 	public SoundController (VisAssetManager manager) {
 		//music is persisted across whole game, so we load it manually
-		String musicPath = "music/boss.ogg";
+		String musicPath = "music/ACDC.mp3";
 		String clickPath = "sound/accept.wav";
 
 		manager.load(musicPath, Music.class);
